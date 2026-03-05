bce25f2560f317f1a20b4ad7e02bb18c668db4ae
deyniloy7 <dey.niloy007@gmail.com>
2026-03-06 05:07:00 +0530

    ci(github-actions): add maven build pipeline

---
+++ src/main/java/com/finstream/finstream_core/FinstreamCoreApplication.java
@@ -0,0 +1,13 @@
+package com.finstream.finstream_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinstreamCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinstreamCoreApplication.class, args);
+	}
+
+}
